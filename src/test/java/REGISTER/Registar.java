af197970611dea7199b9c73211e3a1b03141bdd2
ganeshdhatsan <ganeshdhatsan@gmail.com>
2024-09-14 10:29:42 +0530

    new code commited

---
+++ src/test/java/REGISTER/Registar.java
@@ -0,0 +1,18 @@
+package REGISTER;
+
+import org.testng.annotations.Test;
+
+public class Registar {
+
+	@Test
+	private void createNewRegister() {
+		System.out.println("createNewRegister");
+	}
+
+	@Test
+	private void createRegister() {
+		System.out.println("createRegister");
+
+	}
+
+}
