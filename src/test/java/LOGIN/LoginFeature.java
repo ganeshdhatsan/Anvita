38ac34ecec0aed842cd267c0bf9eff0374287282
ganeshdhatsan <ganeshdhatsan@gmail.com>
2024-09-18 10:30:02 +0530

    New changes updated

---
+++ src/test/java/LOGIN/LoginFeature.java
@@ -0,0 +1,25 @@
+package LOGIN;
+
+
+
+public class LoginFeature {
+
+	public void login() {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	
+	}
+	
+	public void forgotPassword() {
+		System.out.println();
+	}
+	
+	
+	
+
+}
